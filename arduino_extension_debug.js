66845825ab201f0bc8632b2c52417e3e9a023f47
Alec Flett <alecf@flett.org>
2018-05-28 10:36:09 -0700

    create npm package

--- arduino_extension_debug.js
+++ arduino_extension_debug.js
@@ -13,8 +13,8 @@
  *along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-(function(ext) {
-
+(function (ext) {
+  console.log('beginning initialization with ', ext);
   var PIN_MODE = 0xF4,
     REPORT_DIGITAL = 0xD0,
     REPORT_ANALOG = 0xC0,
@@ -43,7 +43,7 @@
 
   var LOW = 0,
     HIGH = 1;
-  
+
   var MAX_DATA_BYTES = 4096;
   var MAX_PINS = 128;
 
@@ -64,7 +64,7 @@
 
   var majorVersion = 0,
     minorVersion = 0;
-  
+
   var connected = false;
   var notifyConnection = false;
   var device = null;
@@ -82,19 +82,19 @@
   function HWList() {
     this.devices = [];
 
-    this.add = function(dev, pin) {
-      var device = this.search(dev);
-      if (!device) {
-        device = {name: dev, pin: pin, val: 0};
-        this.devices.push(device);
+    this.add = function (dev, pin) {
+      var newDevice = this.search(dev);
+      if (!newDevice) {
+        newDevice = { name: dev, pin: pin, val: 0 };
+        this.devices.push(newDevice);
       } else {
-        device.pin = pin;
-        device.val = 0;
+        newDevice.pin = pin;
+        newDevice.val = 0;
       }
     };
 
-    this.search = function(dev) {
-      for (var i=0; i<this.devices.length; i++) {
+    this.search = function (dev) {
+      for (var i = 0; i < this.devices.length; i++) {
         if (this.devices[i].name === dev)
           return this.devices[i];
       }
@@ -114,13 +114,15 @@
     // TEMPORARY WORKAROUND
     // Since _deviceRemoved is not used with Serial devices
     // ping device regularly to check connection
-    pinger = setInterval(function() {
+    pinger = setInterval(function () {
       if (pinging) {
         if (++pingCount > 6) {
           clearInterval(pinger);
           pinger = null;
           connected = false;
           if (device) device.close();
+          delete devicesSeen[device.id];
+          console.log('disconnecting: ', pingCount, 'pings')
           device = null;
           return;
         }
@@ -144,21 +146,22 @@
   }
 
   function queryFirmware() {
+    console.log('queryFirmware on ', device.id);
     var output = new Uint8Array([START_SYSEX, QUERY_FIRMWARE, END_SYSEX]);
     device.send(output.buffer);
   }
- 
+
   function queryCapabilities() {
     console.log('Querying ' + device.id + ' capabilities');
     var msg = new Uint8Array([
-        START_SYSEX, CAPABILITY_QUERY, END_SYSEX]);
+      START_SYSEX, CAPABILITY_QUERY, END_SYSEX]);
     device.send(msg.buffer);
   }
 
   function queryAnalogMapping() {
     console.log('Querying ' + device.id + ' analog mapping');
     var msg = new Uint8Array([
-        START_SYSEX, ANALOG_MAPPING_QUERY, END_SYSEX]);
+      START_SYSEX, ANALOG_MAPPING_QUERY, END_SYSEX]);
     device.send(msg.buffer);
   }
 
@@ -178,11 +181,11 @@
   function processSysexMessage() {
     console.log("Sysex Message:");
     console.log(storedInputData);
-    switch(storedInputData[0]) {
+    switch (storedInputData[0]) {
       case CAPABILITY_RESPONSE:
         for (var i = 1, pin = 0; pin < MAX_PINS; pin++) {
           while (storedInputData[i++] != 0x7F) {
-            pinModes[storedInputData[i-1]].push(pin);
+            pinModes[storedInputData[i - 1]].push(pin);
             i++; //Skip mode resolution
           }
           if (i == sysexBytesRead) break;
@@ -193,21 +196,23 @@
         for (var pin = 0; pin < analogChannel.length; pin++)
           analogChannel[pin] = 127;
         for (var i = 1; i < sysexBytesRead; i++)
-          analogChannel[i-1] = storedInputData[i];
+          analogChannel[i - 1] = storedInputData[i];
         for (var pin = 0; pin < analogChannel.length; pin++) {
           if (analogChannel[pin] != 127) {
             var out = new Uint8Array([
-                REPORT_ANALOG | analogChannel[pin], 0x01]);
+              REPORT_ANALOG | analogChannel[pin], 0x01]);
             device.send(out.buffer);
           }
         }
         notifyConnection = true;
-        setTimeout(function() {
+        setTimeout(function () {
           notifyConnection = false;
         }, 100);
         break;
       case QUERY_FIRMWARE:
+        console.log('got firmware...')
         if (!connected) {
+          console.log('... but not connected, so clearing watchdog')
           clearInterval(poller);
           poller = null;
           clearTimeout(watchdog);
@@ -221,9 +226,10 @@
     }
   }
 
-  function processInput(inputData) { 
-    for (var i=0; i < inputData.length; i++) {
-      
+  function processInput(inputData) {
+    console.log('processInputData(', inputData, ')');
+    for (var i = 0; i < inputData.length; i++) {
+
       if (parsingSysex) {
         if (inputData[i] == END_SYSEX) {
           parsingSysex = false;
@@ -234,7 +240,7 @@
       } else if (waitForData > 0 && inputData[i] < 0x80) {
         storedInputData[--waitForData] = inputData[i];
         if (executeMultiByteCommand !== 0 && waitForData === 0) {
-          switch(executeMultiByteCommand) {
+          switch (executeMultiByteCommand) {
             case DIGITAL_MESSAGE:
               setDigitalInputs(multiByteChannel, (storedInputData[0] << 7) + storedInputData[1]);
               break;
@@ -253,7 +259,7 @@
         } else {
           command = inputData[i];
         }
-        switch(command) {
+        switch (command) {
           case DIGITAL_MESSAGE:
           case ANALOG_MESSAGE:
           case REPORT_VERSION:
@@ -305,9 +311,9 @@
     val = Math.round((val / 100) * 255);
     pinMode(pin, PWM);
     var msg = new Uint8Array([
-        ANALOG_MESSAGE | (pin & 0x0F),
-        val & 0x7F,
-        val >> 7]);
+      ANALOG_MESSAGE | (pin & 0x0F),
+      val & 0x7F,
+      val >> 7]);
     device.send(msg.buffer);
   }
 
@@ -323,9 +329,9 @@
       digitalOutputData[portNum] |= (1 << (pin & 0x07));
     pinMode(pin, OUTPUT);
     var msg = new Uint8Array([
-        DIGITAL_MESSAGE | portNum,
-        digitalOutputData[portNum] & 0x7F,
-        digitalOutputData[portNum] >> 0x07]);
+      DIGITAL_MESSAGE | portNum,
+      digitalOutputData[portNum] & 0x7F,
+      digitalOutputData[portNum] >> 0x07]);
     device.send(msg.buffer);
   }
 
@@ -336,37 +342,37 @@
     }
     pinMode(pin, SERVO);
     var msg = new Uint8Array([
-        ANALOG_MESSAGE | (pin & 0x0F),
-        deg & 0x7F,
-        deg >> 0x07]);
+      ANALOG_MESSAGE | (pin & 0x0F),
+      deg & 0x7F,
+      deg >> 0x07]);
     device.send(msg.buffer);
   }
 
-  ext.whenConnected = function() {
+  ext.whenConnected = function () {
     if (notifyConnection) return true;
     return false;
   };
 
-  ext.analogWrite = function(pin, val) {
+  ext.analogWrite = function (pin, val) {
     analogWrite(pin, val);
   };
 
-  ext.digitalWrite = function(pin, val) {
+  ext.digitalWrite = function (pin, val) {
     if (val == 'on')
       digitalWrite(pin, HIGH);
     else if (val == 'off')
       digitalWrite(pin, LOW);
   };
 
-  ext.analogRead = function(pin) {
+  ext.analogRead = function (pin) {
     return analogRead(pin);
   };
 
-  ext.digitalRead = function(pin) {
+  ext.digitalRead = function (pin) {
     return digitalRead(pin);
   };
 
-  ext.whenAnalogRead = function(pin, op, val) {
+  ext.whenAnalogRead = function (pin, op, val) {
     if (pin >= 0 && pin < pinModes[ANALOG].length) {
       if (op == '>')
         return analogRead(pin) > val;
@@ -379,7 +385,7 @@
     }
   };
 
-  ext.whenDigitalRead = function(pin, val) {
+  ext.whenDigitalRead = function (pin, val) {
     if (hasCapability(pin, INPUT)) {
       if (val == 'on')
         return digitalRead(pin);
@@ -388,11 +394,11 @@
     }
   };
 
-  ext.connectHW = function(hw, pin) {
+  ext.connectHW = function (hw, pin) {
     hwList.add(hw, pin);
   };
 
-  ext.rotateServo = function(servo, deg) {
+  ext.rotateServo = function (servo, deg) {
     var hw = hwList.search(servo);
     if (!hw) return;
     if (deg < 0) deg = 0;
@@ -401,7 +407,7 @@
     hw.val = deg;
   };
 
-  ext.changeServo = function(servo, change) {
+  ext.changeServo = function (servo, change) {
     var hw = hwList.search(servo);
     if (!hw) return;
     var deg = hw.val + change;
@@ -411,14 +417,14 @@
     hw.val = deg;
   };
 
-  ext.setLED = function(led, val) {
+  ext.setLED = function (led, val) {
     var hw = hwList.search(led);
     if (!hw) return;
     analogWrite(hw.pin, val);
     hw.val = val;
   };
 
-  ext.changeLED = function(led, val) {
+  ext.changeLED = function (led, val) {
     var hw = hwList.search(led);
     if (!hw) return;
     var b = hw.val + val;
@@ -428,7 +434,7 @@
     hw.val = b;
   };
 
-  ext.digitalLED = function(led, val) {
+  ext.digitalLED = function (led, val) {
     var hw = hwList.search(led);
     if (!hw) return;
     if (val == 'on') {
@@ -439,14 +445,14 @@
       hw.val = 0;
     }
   };
-  
-  ext.readInput = function(name) {
+
+  ext.readInput = function (name) {
     var hw = hwList.search(name);
     if (!hw) return;
     return analogRead(hw.pin);
   };
 
-  ext.whenButton = function(btn, state) {
+  ext.whenButton = function (btn, state) {
     var hw = hwList.search(btn);
     if (!hw) return;
     if (state === 'pressed')
@@ -455,13 +461,13 @@
       return !digitalRead(hw.pin);
   };
 
-  ext.isButtonPressed = function(btn) {
+  ext.isButtonPressed = function (btn) {
     var hw = hwList.search(btn);
     if (!hw) return;
     return digitalRead(hw.pin);
   };
 
-  ext.whenInput = function(name, op, val) {
+  ext.whenInput = function (name, op, val) {
     var hw = hwList.search(name);
     if (!hw) return;
     if (op == '>')
@@ -474,26 +480,34 @@
       return false;
   };
 
-  ext.mapValues = function(val, aMin, aMax, bMin, bMax) {
+  ext.mapValues = function (val, aMin, aMax, bMin, bMax) {
     var output = (((bMax - bMin) * (val - aMin)) / (aMax - aMin)) + bMin;
     return Math.round(output);
   };
- 
-  ext._getStatus = function() {
+
+  ext._getStatus = function () {
     if (!connected)
-      return { status:1, msg:'Disconnected' };
+      return { status: 1, msg: 'Disconnected' };
     else
-      return { status:2, msg:'Connected' };
+      return { status: 2, msg: 'Connected' };
   };
 
-  ext._deviceRemoved = function(dev) {
+  ext._deviceRemoved = function (dev) {
     console.log('Device removed');
     // Not currently implemented with serial devices
   };
 
   var potentialDevices = [];
-  ext._deviceConnected = function(dev) {
+  var devicesSeen = {};
+  ext._deviceConnected = function (dev) {
+    if (dev.id in devicesSeen) {
+      console.log('I have already seen', dev);
+      return;
+    }
+    console.log('_deviceConnected: ', dev)
+    // console.trace();
     potentialDevices.push(dev);
+    devicesSeen[dev.id] = dev;
     if (!device)
       tryNextDevice();
   };
@@ -501,36 +515,47 @@
   var poller = null;
   var watchdog = null;
   function tryNextDevice() {
+    console.log('trying next device (1/', potentialDevices.length, ')');
     device = potentialDevices.shift();
     if (!device) return;
 
-    device.open({ stopBits: 0, bitRate: 57600, ctsFlowControl: 0 });
-    console.log('Attempting connection with ' + device.id);
-    device.set_receive_handler(function(data) {
-      var inputData = new Uint8Array(data);
-      console.log("Input Data:");
-      console.log(inputData);
-      processInput(inputData);
-    });
+    device.open({ stopBits: 0, bitRate: 57600, ctsFlowControl: 0 },
+      function () {
+        console.log('opened... Attempting connection with ' + device.id);
+        device.set_receive_handler(function (data) {
+          console.log("Input Data:");
+          console.log(inputData);
+          var inputData = new Uint8Array(data);
+          processInput(inputData);
+        });
+        console.log('...clearing watchdog')
+        clearTimeout(watchdog);
+        watchdog = null;
 
-    poller = setInterval(function() {
-      queryFirmware();
-    }, 1000);
-
-    watchdog = setTimeout(function() {
+        queryFirmware()
+        poller = setInterval(function () {
+          queryFirmware();
+        }, 1000);
+      });
+    watchdog = setTimeout(function () {
+      console.log('timeout waiting for ', device.id)
       clearInterval(poller);
       poller = null;
       device.set_receive_handler(null);
       device.close();
+      console.log('timeout accessing ', device.id);
+      delete devicesSeen[device.id];
       device = null;
       tryNextDevice();
     }, 5000);
   }
 
-  ext._shutdown = function() {
+  ext._shutdown = function () {
     // TODO: Bring all pins down 
+    console.trace('Shutting down..', device.id)
     if (device) device.close();
     if (poller) clearInterval(poller);
+    delete devicesSeen[device.id];
     device = null;
   };
 
@@ -538,9 +563,9 @@
   var paramString = window.location.search.replace(/^\?|\/$/g, '');
   var vars = paramString.split("&");
   var lang = 'en';
-  for (var i=0; i<vars.length; i++) {
+  for (var i = 0; i < vars.length; i++) {
     var pair = vars[i].split('=');
-    if (pair.length > 1 && pair[0]=='lang')
+    if (pair.length > 1 && pair[0] == 'lang')
       lang = pair[1];
   }
 
@@ -760,7 +785,7 @@
       outputs: ['on', 'off'],
       ops: ['>', '=', '<'],
       servos: ['servo A', 'servo B', 'servo C', 'servo D']
-    },  
+    },
     de: {
       buttons: ['Taste A', 'Taste B', 'Taste C', 'Taste D'],
       btnStates: ['gedrückt', 'losgelassen'],
@@ -829,6 +854,7 @@
     url: 'http://khanning.github.io/scratch-arduino-extension'
   };
 
-  ScratchExtensions.register('Arduino Debug', descriptor, ext, {type:'serial'});
+  console.log('registering extension with ', ScratchExtensions);
+  ScratchExtensions.register('Arduino Debug', descriptor, ext, { type: 'serial' });
 
 })({});
